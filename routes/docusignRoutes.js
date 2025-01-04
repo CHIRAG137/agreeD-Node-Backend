a732fd830dd46ab2641026c1f904180646a90170
CHIRAG137 <chiraggoel138@gmail.com>
2025-01-05 02:42:19 +0530

    Add: APIs to create docusign envelopes

---
+++ routes/docusignRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const envelopeController = require('../controllers/docusignController');
+
+// Route to create and send the envelope
+router.post('/create-envelope', envelopeController.createAndSendEnvelope);
+
+// Route to generate recipient view (URL for the signer to sign)
+router.post('/create-recipient-view/:envelopeId', envelopeController.createRecipientView);
+
+module.exports = router;
